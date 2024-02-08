9bf6c09a1bcc59cc9c8147cc226bc2b981c066e6
archiecheng <archiechengice@outlook.com>
2024-02-08 13:50:00 -0800

    列表数据获取渲染

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import "./style.css";
 import { Tabs } from "antd-mobile";
 import { useTabs } from './useTabs'
+import HomeList from "./HomeList";
 const Home = () => {
   const { channels } = useTabs()
   return (
@@ -11,6 +12,7 @@ const Home = () => {
           {channels.map((item) => (
             <Tabs.Tab title={item.name} key={item.id}>
               {/* list 组件 */}
+              <HomeList channelId ={item.id + ''}/>
             </Tabs.Tab>
           ))}
         </Tabs>
