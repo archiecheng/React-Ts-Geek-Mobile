01733eaa21ccae56fc2c8ac9f1bce114da7493a8
archiecheng <archiechengice@outlook.com>
2024-02-08 14:34:19 -0800

    详情页面跳转和渲染

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,5 +1,43 @@
-const Detail = () => {
-    return <div>this is Detail</div>
-}
+import { DetailDataType, fetchDetailAPI } from "@/apis/detail";
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { NavBar } from "antd-mobile";
 
-export default Detail
+const Detail = () => {
+  const [detail, setDetail] = useState<DetailDataType | null>(null);
+
+  //   获取路由参数
+  const [params] = useSearchParams();
+  const id = params.get("id");
+  useEffect(() => {
+    const getDetail = async () => {
+      try {
+        const res = await fetchDetailAPI(id!);
+        setDetail(res.data.data)
+      } catch (error) {
+        throw new Error("fetch detail error");
+      }
+    };
+    getDetail()
+  }, [id]);
+
+  const navigate = useNavigate()
+  const back = () => {
+    navigate(-1)
+  }
+
+  // 数据返回前用loading渲染
+  if (!detail) {
+    return <div>loading....</div>
+  }
+
+  // 数据返回后，正式渲染内容
+  return <div>
+    <NavBar onBack={back}>{detail?.title}</NavBar>
+    <div dangerouslySetInnerHTML={{
+      __html:detail?.content
+    }}></div>
+  </div>;
+};
+
+export default Detail;
