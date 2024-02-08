e86c018eb2862053adcd8c968dd66203a5900d87
archiecheng <archiechengice@outlook.com>
2024-02-08 12:15:17 -0800

    路由配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { Button } from 'antd-mobile'
 const App = () => {
-  return <div>this is app</div>
+  return <div>this is app<Button color='success'>click me</Button></div>
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+// 导入 provider
+import { RouterProvider } from 'react-router-dom'
+import { router } from "./router/index.tsx";
+ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
