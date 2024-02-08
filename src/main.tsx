4e2c2ab47dff4f8474df819d47e6cea64019fa37
archiecheng <archiechengice@outlook.com>
2024-02-08 12:46:11 -0800

    axios配合ts使用

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,12 @@ import "./index.css";
 // 导入 provider
 import { RouterProvider } from 'react-router-dom'
 import { router } from "./router/index.tsx";
+
+// 测试接口
+
+import { fetchChannelAPI } from '@/apis/list.ts'
+fetchChannelAPI().then(res => {
+    console.log(res);
+    
+})
 ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
